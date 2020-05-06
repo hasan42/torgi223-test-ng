afb4e4cc4ee74f785e85813c02e1dcbe7029ed59
rpokosov <hasan83842@gmail.com>
2020-05-06 13:42:00 +0300

    create components, add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommentComponent } from './shared/components/comment/comment.component';
+import { AddCommentComponent } from './shared/components/add-comment/add-comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentComponent,
+    AddCommentComponent
   ],
   imports: [
     BrowserModule
